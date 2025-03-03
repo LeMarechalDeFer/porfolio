f6e08c7057a8c30cf46e8893aa1a53a895ebd073
LeMarechalDeFer <blanchot@et.esiea.fr>
2025-03-03 16:01:10 +0700

    proxy 2

--- portfolio-app/next.config.ts
+++ portfolio-app/next.config.ts
@@ -4,12 +4,26 @@ const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
+      // {
+      //   source: '/ingest/:path*',
+      //   destination: 'https://app.posthog.com/:path*',
+      // },
       {
-        source: '/ingest/:path*',
-        destination: 'https://app.posthog.com/:path*',
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
       },
     ];
   },
+  skipTrailingSlashRedirect: true, 
+
 };
 
 export default nextConfig;
